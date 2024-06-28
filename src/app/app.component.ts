30ba9d180c68fcd728aad6ac165b8c3155189aa0
Bernardo Amaral <dev02@rksoftware.com.br>
2024-06-28 17:22:55 -0300

    Add components and loaders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { AppMainContainerComponent } from './components/app-main-container/app-main-container.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { AppMainContainerComponent } from './components/app-main-container/app-m
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
+    MatProgressSpinnerModule,
     AppMainContainerComponent,
   ],
   templateUrl: './app.component.html',
